d27ccdd2541052dd9fa75d398c70dfcfb1a4da3a
Sam Hawke <shawke@m-shawke-25.skidmore.edu>
2026-07-23 12:08:29 -0400

    added democracy experiment

--- experiments/democracy_confidence_set.R
+++ experiments/democracy_confidence_set.R
@@ -1,11 +1,13 @@
 source("methods/confidence_sets_bic_relative.R")
+library(lavaan)
+data("PoliticalDemocracy")
 
 set.seed(12)
 
-max_k <- 8
+max_k <- 6
 B <- 400
 alphas <- c(0.01, 0.05, 0.1, 0.2)
 
-X <- holzinger19
+X <- PoliticalDemocracy
 conf_sets <- get_confidence_sets_bic_relative(X, max_k, B=B, alphas=alphas, verbose=T)
 print(conf_sets)
